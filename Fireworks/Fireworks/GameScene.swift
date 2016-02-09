90c73c5306da4de2715eb53cb8bff5eef96b47f2
Kevia <cloud@kevia.me>
2016-02-09 09:54:30 -0800

    added basic firework animation

--- Fireworks/Fireworks/GameScene.swift
+++ Fireworks/Fireworks/GameScene.swift
@@ -32,7 +32,7 @@ class GameScene: SKScene
         background.zPosition = -1
         addChild(background)
         
-        //gameTimer = NSTimer.scheduledTimerWithTimeInterval(6, target: self, selector: "launchFireworks", userInfo: nil, repeats: true)
+        gameTimer = NSTimer.scheduledTimerWithTimeInterval(6, target: self, selector: "launchFireworks", userInfo: nil, repeats: true)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?)
@@ -47,6 +47,86 @@ class GameScene: SKScene
     
     func launchFireworks()
     {
+        let movementAmount: CGFloat = 1800
         
+        switch GKRandomSource.sharedRandom().nextIntWithUpperBound(4)
+        {
+            case 0:
+            // fire five, straight up
+                createFirework(xMovement: 0, x: 512, y: bottomEdge)
+                createFirework(xMovement: 0, x: 512 - 200, y: bottomEdge)
+                createFirework(xMovement: 0, x: 512 - 100, y: bottomEdge)
+                createFirework(xMovement: 0, x: 512 + 100, y: bottomEdge)
+                createFirework(xMovement: 0, x: 512 + 200, y: bottomEdge)
+            
+            case 1:
+            // fire five, in a fan
+                createFirework(xMovement: 0, x: 512, y: bottomEdge)
+                createFirework(xMovement: -200, x: 512 - 200, y: bottomEdge)
+                createFirework(xMovement: -100, x: 512 - 100, y: bottomEdge)
+                createFirework(xMovement: 100, x: 512 + 100, y: bottomEdge)
+                createFirework(xMovement: 200, x: 512 + 200, y: bottomEdge)
+            
+            case 2:
+            // fire five, from the left to the right
+                createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 400)
+                createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 300)
+                createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 200)
+                createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 100)
+                createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge)
+            
+            case 3:
+            // fire five, from the right to the left
+                createFirework(xMovement: -movementAmount, x: rightEdge, y: bottomEdge + 400)
+                createFirework(xMovement: -movementAmount, x: rightEdge, y: bottomEdge + 300)
+                createFirework(xMovement: -movementAmount, x: rightEdge, y: bottomEdge + 200)
+                createFirework(xMovement: -movementAmount, x: rightEdge, y: bottomEdge + 100)
+                createFirework(xMovement: -movementAmount, x: rightEdge, y: bottomEdge)
+            
+            default:
+                break
+        }
+    }
+    
+    func createFirework(xMovement xMovement: CGFloat, x: Int, y: Int)
+    {
+        let node = SKNode()
+        node.position = CGPoint(x: x, y: y)
+        
+        let firework = SKSpriteNode(imageNamed: "rocket")
+        firework.name = "firework"
+        node.addChild(firework)
+        
+        switch GKRandomSource.sharedRandom().nextIntWithUpperBound(3)
+        {
+            case 0:
+                firework.color = UIColor.cyanColor()
+                firework.colorBlendFactor = 1
+            
+            case 1:
+                firework.color = UIColor.greenColor()
+                firework.colorBlendFactor = 1
+            
+            case 2:
+                firework.color = UIColor.redColor()
+                firework.colorBlendFactor = 1
+            
+            default:
+                break
+        }
+        
+        let path = UIBezierPath()
+        path.moveToPoint(CGPoint(x: 0, y: 0))
+        path.addLineToPoint(CGPoint(x: xMovement, y: 1000))
+        
+        let move = SKAction.followPath(path.CGPath, asOffset: true, orientToPath: true, speed: 200)
+        node.runAction(move)
+        
+        let emitter = SKEmitterNode(fileNamed: "fuse")!
+        emitter.position = CGPoint(x: 0, y: -22)
+        node.addChild(emitter)
+        
+        fireworks.append(node)
+        addChild(node)
     }
 }
